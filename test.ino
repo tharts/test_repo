013ec73e6ceb72f4e6ff126dbc7a1a68606d538d
Thomas Arts <thomas@artsonline.de>
2018-01-10 12:50:42 +0100

    And now

---
+++ test.ino
@@ -0,0 +1,16 @@
+#if USE_MQTT == 1
+  #include <PubSubClient.h>
+  //Your MQTT Broker
+  const char* mqtt_server = "192.168.192.xyz";
+  const char* mqtt_topic = "socket/switch2";
+
+#endif
+
+
+//Yout Wifi SSID
+const char* ssid = "SomeSSID";
+//Your Wifi Key
+const char* password = "A very secret passphrase";
+
+// another change .....
+
